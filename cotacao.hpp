fedee2e8f313b5d82d08a5d85658ef4e43eab488
Letícia Lauriano De Oliveira <132012576+lelauriano@users.noreply.github.com>
2025-06-14 13:41:50 -0300

    Update cotacao.hpp

---
+++ cotacao.hpp
@@ -0,0 +1,20 @@
+#ifndef COTACAO_HPP
+#define COTACAO_HPP
+
+#include <string>
+
+class Cotacao {
+private:
+    std::string data;
+    double valor;
+
+public:
+    Cotacao(const std::string& data, double valor);
+
+    std::string getData() const;
+    double getValor() const;
+
+    void setValor(double valor);
+};
+
+#endif
