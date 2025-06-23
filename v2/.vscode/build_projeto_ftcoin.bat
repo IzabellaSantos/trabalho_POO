31906d510522dc71efd8ec271cad785a39bf2d5c
neobaobr <36973680+neobaobr@users.noreply.github.com>
2025-06-23 20:57:04 -0300

    Update build_projeto_ftcoin.bat

--- v2/.vscode/build_projeto_ftcoin.bat
+++ v2/.vscode/build_projeto_ftcoin.bat
@@ -3,12 +3,12 @@ cd /d %~dp0
 
 echo Compilando todos os arquivos do projeto...
 
-g++ ^
-Main.cpp Menu.cpp ^
-model\Carteira.cpp model\Movimentacao.cpp model\Cotacao.cpp ^
-controllers\CarteiraController.cpp controllers\MovimentacaoController.cpp controllers\OraculoController.cpp controllers\RelatorioController.cpp ^
-dao\CarteiraInMemoryDAO.cpp dao\MovimentacaoInMemoryDAO.cpp dao\OraculoInMemoryDAO.cpp ^
-simular_cotacoes.cpp ^
+g++ 
+Main.cpp Menu.cpp 
+model\Carteira.cpp model\Movimentacao.cpp model\Cotacao.cpp 
+controllers\CarteiraController.cpp controllers\MovimentacaoController.cpp controllers\OraculoController.cpp controllers\RelatorioController.cpp 
+dao\CarteiraInMemoryDAO.cpp dao\MovimentacaoInMemoryDAO.cpp dao\OraculoInMemoryDAO.cpp 
+simular_cotacoes.cpp 
 -o ftcoin.exe -std=c++11
 
 if %errorlevel% neq 0 (
